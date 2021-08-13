b847b97c66a12bceeb4b70db06a3e4e8a3231cb5
unknown <yeasin.mohammod018@gmail.com>
2021-08-14 01:36:25 +0600

    js file ready

--- converter.js
+++ converter.js
@@ -1,5 +1,14 @@
 document.getElementById('submit').addEventListener('click', heightConverter);
 
 function heightConverter () {
+    var feet = parseInt(document.getElementById('feet').value * 12);
+    var inches = parseInt(document.getElementById('inches').value);
+    // console.log(feet);
+    // console.log(inches);
+    var cm = ((feet + inches) * 2.54);
     
+    // console.log(cm);
+    var result = cm.toFixed(2);
+
+    document.getElementById('result').innerHTML = result + " cm";
 }
